b9f0b59a297e6be02643dda229958b811189ea8c
“Pratik <pratikpatil6674@gmail.com>
2026-04-20 23:47:25 +0900

    Update resources

--- src/resources.py
+++ src/resources.py
@@ -6,3 +6,22 @@ COPY_ICON = QIcon(":/resources/icons/copy.svg")
 DELETE_ICON = QIcon(":/resources/icons/delete.svg")
 EDIT_ICON = QIcon(":/resources/icons/edit.svg")
 FAV_ICON = QIcon(":/resources/icons/fav.svg")
+
+COPY_ICON_LIGHT = QIcon(":/resources/icons/copy_light.svg")
+DELETE_ICON_LIGHT = QIcon(":/resources/icons/delete_light.svg")
+EDIT_ICON_LIGHT = QIcon(":/resources/icons/edit_light.svg")
+
+CLIP_ICON = QIcon(":/resources/icons/clip_dark.svg")
+STAR_ICON = QIcon(":/resources/icons/star_dark.svg")
+NOTE_ICON = QIcon(":/resources/icons/note_dark.svg")
+TRANSLATE_ICON = QIcon(":/resources/icons/translate_dark.svg")
+AI_ICON = QIcon(":/resources/icons/ai_dark.svg")
+SETTINGS_ICON = QIcon(":/resources/icons/settings_dark.svg")
+
+# tab light icons
+CLIP_ICON_LIGHT = QIcon(":/resources/icons/clip_light.svg")
+STAR_ICON_LIGHT = QIcon(":/resources/icons/star_light.svg")
+NOTE_ICON_LIGHT = QIcon(":/resources/icons/note_light.svg")
+TRANSLATE_ICON_LIGHT = QIcon(":/resources/icons/translate_light.svg")
+AI_ICON_LIGHT = QIcon(":/resources/icons/ai_light.svg")
+SETTINGS_ICON_LIGHT = QIcon(":/resources/icons/settings_light.svg")
